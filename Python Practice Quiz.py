9fdc53bd118f52ff5491b979f16fc27cb4196917
CaseyGann <cmoden@gannacademy.org>
2023-09-12 17:31:19 -0400

    init

---
+++ Python Practice Quiz.py
@@ -0,0 +1,9 @@
+def sorter(L1, L2):
+    #implementation not shown
+    pass
+
+def main():
+    fruits = ["Apple", "Banana", "Cherry"]
+    vegetables = ["Asparagus", "Beetroot", "Carrot"]	
+    print(sorter(fruits, vegetables))
+
